aa7f6280b72b007a749ca1fe002653482e4fad41
Mathew Zamacona <mateoszamacona@icloud.com>
2024-07-26 10:13:49 -0600

    add readme and yaml file

--- src/DogConverter.tsx
+++ src/DogConverter.tsx
@@ -2,23 +2,29 @@ import React, { useState } from 'react';
 import dogImage from './assets/Dog1.jpeg';
 import dog3 from './assets/dog-paw.jpeg';
 
+// creating the component and making it type Reac.FC
 const DogConverter: React.FC = () => {
+    // setting use state to number and string
     const [age, setAge] = useState<number | string>("");
+    // set use state to string and null 
     const [dogage, setDogAge] = useState<string | null>(null);
-
+    // When someone types a number into the input div all of the work 
+    // is handled in this function and returns a value 
     const handleAgeInput = (event: React.ChangeEvent<HTMLInputElement>): void => {
         const value = event.target.valueAsNumber;
         setAge(isNaN(value) ? "" : value);
     }
-
+    // When Someone clicks on enter it will go here and handle the work below 
     const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
+        // prevent default so that the setDogAge or the final result is not reset automatically and stays on the page 
         event.preventDefault();
         const ageNumber = Number(age);
-
         if (!isNaN(ageNumber) && ageNumber > 0) {
+            // secret sauce formula rite here to convert human age to dog age
             const dogAgeCalculation = 16 * Math.log(ageNumber) + 31;
             setDogAge(dogAgeCalculation.toFixed(2));
         } else {
+            // if its not satisfied display nothing 
             setDogAge(null);
         }
     }
@@ -37,11 +43,13 @@ const DogConverter: React.FC = () => {
                     id="age"
                     type="number"
                     value={age}
+                    // when someone types something it gets sent to the function in the curly braces
                     onChange={handleAgeInput}
                     placeholder="0" />
             </div>
             <div className="enter">
                 <button
+                    // when someone clicks the enter button its basically the event listener for handleclick below 
                     onClick={handleClick}
                     type="submit"
                     className="submit"
@@ -55,7 +63,6 @@ const DogConverter: React.FC = () => {
                         <div className='direct-info' >A {age} year-old human is approximately {dogage} years old in dog years.</div>
                     </div>
                 )}
-                {/* <img src={dog3} alt='dog3'></img> */}
             </div>
         </form>
     );
